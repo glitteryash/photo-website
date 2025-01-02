d1fd89f38d5362876ef062a2e9cda7ec5f137296
desk-059 <hacchobori-05@desk-059.local>
2025-01-02 17:57:13 +0900

    setting fix

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "../components/Search";
 import Picture from "../components/Picture";
 
@@ -6,25 +6,34 @@ const Homepage = () => {
   const [input, setInput] = useState("");
   const searchUrl = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=1`;
 
+  //search function, fetches data from the API
   const [data, setdata] = useState(null);
   const pexelsAuth = "gYrElvR5Y4ZnMfcqY9MgYQWGi99e6NGuMWmOrnenc7lJBQztbqEZ9mZg";
   const initialUrl = `https://api.pexels.com/v1/curated?per_page=15&page=1`;
-  const search = async () => {
-    const dataFetch = await fetch(initialUrl, {
+  const search = async (url) => {
+    const dataFetch = await fetch(url, {
       method: "GET",
       headers: {
         Accept: "application/json",
-        authorization: pexelsAuth,
+        Authorization: pexelsAuth,
       },
     });
     const parseData = await dataFetch.json();
     setdata(parseData.photos);
-    console.log(parseData);
   };
+  //useEffect to call the search function when the page loads
+  useEffect(() => {
+    search(initialUrl);
+  }, []);
 
   return (
     <div style={{ minHeight: "100vh" }}>
-      <Search search={search} />
+      <Search
+        search={() => {
+          search(searchUrl);
+        }}
+        setInput={setInput}
+      />
       <div className="pictures">
         {data &&
           data.map((d) => {
